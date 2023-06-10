fcce7b415dfed50d3afbb9ac5a7a19ca5978b710
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2023-06-10 14:24:37 -0400

    Fix `args` index problem.

--- auxiliar/parser.py
+++ auxiliar/parser.py
@@ -2,27 +2,27 @@
 class CommandParser:
     def __init__(self):
         self.commands = {
-            'add': self.parse_add,
-            'delete': self.parse_delete,
-            'list': self.parse_list,
-            'add-tags': self.parse_add_tags,
-            'delete-tags': self.parse_delete_tags,
-            'get': self.parse_get
+            'add': self._parse_add,
+            'delete': self._parse_delete,
+            'list': self._parse_list,
+            'add-tags': self._parse_add_tags,
+            'delete-tags': self._parse_delete_tags,
+            'get': self._parse_get
         }
 
     def __call__(self, args):
-        inst = self.split(args)
+        inst = self._split(args)
         if inst[0] in self.commands:
             return self.commands[inst[0]](inst[1:])
         else:
             raise Exception('syntax error: unknown command %s', inst[0])
 
-    def parse_add(self, args):
+    def _parse_add(self, args):
         if '-f' in args and '-t' in args:
             try:
                 idx = args.index('-t')
-                files = args[:idx]
-                tags = args[idx+1:]
+                files = args[1:idx]
+                tags = args[idx + 1:]
                 if not len(files) or not len(tags):
                     raise Exception('syntax error: <file-list> and <tag-list> must have files and tags to store')
             except:
@@ -31,11 +31,11 @@ class CommandParser:
         else:
             raise Exception('syntax error: <add> <-f> <file-list> <-t> <tag-list>')
 
-    def parse_delete(self, args):
+    def _parse_delete(self, args):
         if '-q' in args:
             try:
                 idx = args.index('-q')
-                tags = args[idx+1:]
+                tags = args[idx + 1:]
                 if not len(tags):
                     raise Exception("syntax error: tags can't be empty")
             except:
@@ -44,11 +44,11 @@ class CommandParser:
         else:
             raise Exception('syntax error: <delete> <-q> <tag-query>')
 
-    def parse_list(self, args):
+    def _parse_list(self, args):
         if '-q' in args:
             try:
                 idx = args.index('-q')
-                tags = args[idx+1:]
+                tags = args[idx + 1:]
                 if not len(tags):
                     raise Exception("syntax error: tags can't be empty")
             except:
@@ -57,12 +57,12 @@ class CommandParser:
         else:
             raise Exception('syntax error: <list> <-q> <tag-query>')
 
-    def parse_add_tags(self, args):
+    def _parse_add_tags(self, args):
         if '-q' in args and '-t' in args:
             try:
                 idx = args.index('-t')
-                query = args[:idx]
-                tags = args[idx+1:]
+                query = args[1: idx]
+                tags = args[idx + 1:]
                 if not len(query) or not len(tags):
                     raise Exception('syntax error: <tag-query> and <tag-list> must have files and tags to store')
             except:
@@ -71,12 +71,12 @@ class CommandParser:
         else:
             raise Exception('syntax error: <add-tags> <-q> <tag-query> <-t> <tag-list>')
 
-    def parse_delete_tags(self, args):
+    def _parse_delete_tags(self, args):
         if '-q' in args and '-t' in args:
             try:
                 idx = args.index('-t')
-                query = args[:idx]
-                tags = args[idx+1:]
+                query = args[1: idx]
+                tags = args[idx + 1:]
                 if not len(query) or not len(tags):
                     raise Exception('syntax error: <tag-query> and <tag-list> must have files and tags to store')
             except:
@@ -85,11 +85,11 @@ class CommandParser:
         else:
             raise Exception('syntax error: <delete-tags> <-q> <tag-query> <-t> <tag-list>')
 
-    def parse_get(self, args):
+    def _parse_get(self, args):
         if '-q' in args:
             try:
                 idx = args.index('-q')
-                tags = args[idx+1:]
+                tags = args[idx + 1:]
                 if not len(tags):
                     raise Exception("syntax error: tags can't be empty")
             except:
@@ -98,5 +98,5 @@ class CommandParser:
         else:
             raise Exception('syntax error: <get> <-q> <tag-query>')
 
-    def split(self, args):
+    def _split(self, args):
         return args.split()
