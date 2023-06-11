7e3f0341985a1ce022d22a86ca4b0b24feae9114
Hansel Blanco <ernestoblancomalmierca@gmail.com>
2023-06-11 19:58:17 -0400

    Fix infinite recurssion problem with `ply`.

--- instruction_parser/ply_parser.py
+++ instruction_parser/ply_parser.py
@@ -10,16 +10,44 @@ from command import Add, AddTags, Delete, DeleteTags, Get, List
 tokens = (
     'FILENAME',
     'WORD',
+    'ADD',
+    'ADD_TAGS',
+    'DELETE_TAGS',
+    'DELETE',
+    'LIST',
+    'GET',
+    'F',
+    'T',
+    'Q'
 )
 
 # Define the lexer rules
 def t_FILENAME(t):
-    r'[^\s/]+'
+    r'[^\s/]+\.[a-zA-Z0-9_-]+'
     return t
 
 def t_WORD(t):
     r'[a-zA-Z0-9_-]+'
     return t
+
+def t_F(t):
+    r'-f'
+    return t
+
+def t_T(t):
+    r'-t'
+    return t
+
+def t_Q(t):
+    r'-q'
+    return t
+
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    raise Exception(f"Invalid token '{t.value[0]}' at line {t.lineno} (Index {t.lexpos}).")
 
 # Define the parser rules
 def p_input(p):
@@ -34,27 +62,27 @@ def p_instruction(p):
     p[0] = p[1]
 
 def p_add_inst(p):
-    'add_inst : ADD file_list tag_list'
-    file_list = p[2]
-    tag_list = p[3]
+    'add_inst : ADD F file_list T tag_list'
+    file_list = p[3]
+    tag_list = p[-1]
     p[0] = Add(file_list, tag_list)
 
 def p_query_inst(p):
-    'query_inst : inst tag_query'
+    'query_inst : inst Q tag_query'
     command = p[1]
-    tag_query = p[2]
+    tag_query = p[-1]
     p[0] = command(tag_query)
 
 def p_add_tags_inst(p):
-    'add_tags_inst : ADD_TAGS tag_query tag_list'
-    tag_query = p[2]
-    tag_list = p[3]
+    'add_tags_inst : ADD_TAGS Q tag_query T tag_list'
+    tag_query = p[3]
+    tag_list = p[-1]
     p[0] = AddTags(tag_query, tag_list)
 
 def p_delete_tags_inst(p):
-    'delete_tags_inst : DELETE_TAGS tag_query tag_list'
-    tag_query = p[2]
-    tag_list = p[3]
+    'delete_tags_inst : DELETE_TAGS Q tag_query T tag_list'
+    tag_query = p[3]
+    tag_list = p[-1]
     p[0] = DeleteTags(tag_query, tag_list)
 
 def p_inst(p):
